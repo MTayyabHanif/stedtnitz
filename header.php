e402b29404697f6ea7e2b12342895977a3e94134
M Awais Hanif <mawaishanif@hotmail.com>
2017-08-08 22:33:49 +0500

    Added new options for customizer for that I have to modify header and footer templates

--- header.php
+++ header.php
@@ -17,7 +17,9 @@
 	</head>
 	<body <?php body_class('page_pilling_vc'); ?>>
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip header and navigation, jump to content', 'stedtnitz' ); ?></a>
+	<?php $header_class = get_theme_mod('stedtnitz_header_bar_color', 'light' ); ?>
 <header class="top-header">
+	<div class="header <?php echo $header_class; ?>">
 	<?php 
 	if (has_custom_logo()) {
 		the_custom_logo();
@@ -29,6 +31,7 @@
 		<?php 
 	}
 	?>
+	</div>
 </header>
 	<div id="content" class="site-content">
 	<div id="search-icon" class="showsearch"><i class="icono-search"></i></div>
@@ -47,10 +50,12 @@ $menu_light = false;
 if ($menu_light) {
  	$menu_color = 'light-menu';
  }else{$menu_color = '';} ?>
-	<div id="menu-icon" class="". $menu_color ."">
-		<div class="top bar"></div>
-		<div class="middle bar"></div>
-		<div class="bottom bar"></div>
+ 	<div class="menu-container <?php echo get_theme_mod('stedtnitz_menu_icon', 'light'); ?>">
+		<div id="menu-icon" class="". $menu_color ."">
+			<div class="top bar"></div>
+			<div class="middle bar"></div>
+			<div class="bottom bar"></div>
+		</div>
 	</div>
 	<div id="header_nav_menu" class="". $menu_color ."">
 		<?php
--- inc/customizer.php
+++ inc/customizer.php
@@ -28,14 +28,32 @@ function stedtnitz_customize_register( $wp_customize ) {
 			'selector'        => '.site-info',
 			'render_callback' => 'stedtnitz_customize_partial_footer_text',
 		) );
+		$wp_customize->selective_refresh->add_partial( 'stedtnitz_header_bar_color', array(
+	        'selector' => '.top-header',
+	        'render_callback' => 'stedtnitz_header_bar_color_callback',
+    	) );
+    	$wp_customize->selective_refresh->add_partial( 'stedtnitz_footer_color', array(
+	        'selector' => '.site-footer',
+	        'render_callback' => 'stedtnitz_footer_color_callback',
+    	) );
+    	$wp_customize->selective_refresh->add_partial( 'stedtnitz_menu_icon', array(
+	        'selector' => '.menu-container',
+	        'render_callback' => function(){?>
+	        <div id="menu-icon" class="<?php echo get_theme_mod('stedtnitz_menu_icon', 'light'); ?>">
+				<div class="top bar"></div>
+				<div class="middle bar"></div>
+				<div class="bottom bar"></div>
+			</div>
+	        <?php
+	        },
+    	) );
 	}
-
-	$wp_customize->add_setting( 'footer_text', array(
-	  'type' => 'theme_mod',
-	  'capability' => 'edit_theme_options',
-	  'default' => 'Powered By Wordpress',
-	  'transport' => 'postMessage', // or postMessage
-	) );
+		$wp_customize->add_setting( 'footer_text', array(
+		  'type' => 'theme_mod',
+		  'capability' => 'edit_theme_options',
+		  'default' => 'Powered By Wordpress',
+		  'transport' => 'postMessage', // or postMessage
+		) );
 
 	$wp_customize->add_control( 'footer_text', array(
 	  'type' => 'textarea',
@@ -66,17 +84,14 @@ function stedtnitz_customize_register( $wp_customize ) {
 	$wp_customize->add_control( 'stedtnitz_sidebar_options', array(
 	  	'type' => 'select',
 	  	'choices' => array(
-	  	                   'sidebar_1' => 'Blog View - One Post Sidebar', 
-	  	                   'sidebar_2' => 'Blog View - WP Sidebar', 
-	  	                   'no_sidebar' => 'Blog View - No Sidebar'
+	  	                   'sidebar_1' => __('Blog View - One Post Sidebar', 'stedtnitz'), 
+	  	                   'sidebar_2' => __('Blog View - WP Sidebar', 'stedtnitz') ,
+	  	                   'no_sidebar' => __('Blog View - No Sidebar','stedtnitz'),
 	  	          ),
 	  	'priority' => 10, // Within the section.
 	  	'section' => 'stedtnitz_sidebar', // Required, core or custom.
 	  	'label' => __( 'Select Sidebar' ),
 	  	'description' => __( 'Choose the sidebar option for your blog listing page.' ),
-	  	'input_attrs' => array(
-	  		'placeholder' => __( 'Add footer text here.' ),
-	  	),
 	) );
 
 	$wp_customize->add_section( 'stedtnitz_sidebar', array(
@@ -88,36 +103,64 @@ function stedtnitz_customize_register( $wp_customize ) {
 	) );
 	$wp_customize->get_section( 'stedtnitz_sidebar' )->active_callback = 'stedtnitz_is_blog';
 
+	$wp_customize->add_setting( 'stedtnitz_header_bar_color', array(
+	  'type' => 'theme_mod',
+	  'capability' => 'edit_theme_options',
+	  'default' => 'light',
+	  'transport' => 'postMessage', // or postMessage
+	) );
+	$wp_customize->add_control('stedtnitz_header_bar_color', array(
+		'type' => 'select',
+		'choices' => array(
+			'light' => __('Light', 'stedtnitz'),
+			'dark' 	=> __('Dark', 'stedtnitz'),
+			),
+		'priority' => 10, // Within the section.
+	  	'section' => 'colors', // Required, core or custom.
+	  	'label' => __( 'Select Header Bar Color Scheme', 'stedtnitz' ),
+	  	'description' => __( 'Chose the header bar color scheme for all webpages.', 'stedtnitz' ),	
+	) );
 
-	$wp_customize->add_setting(
-	'tcx_link_color',
-		array(
-			'default'     => '#3eacd6',
-			'transport'   => 'postMessage'
-		)
-	);
-	$wp_customize->add_control(
-          new WP_Customize_Color_Control(
-                      $wp_customize,
-                      'link_color',
-                      array(
-                            'label'      => __( 'Link Color', 'tcx' ),
-                            'section'    => 'colors',
-                            'settings'   => 'tcx_link_color'
-                            )
-                      )
-            );
+	$wp_customize->add_setting( 'stedtnitz_footer_color', array(
+	  'type' => 'theme_mod',
+	  'capability' => 'edit_theme_options',
+	  'default' => 'light',
+	  'transport' => 'postMessage', // or postMessage
+	) );
+	$wp_customize->add_control('stedtnitz_footer_color', array(
+		'type' => 'select',
+		'choices' => array(
+			'light' => __('Light', 'stedtnitz'),
+			'dark' 	=> __('Dark', 'stedtnitz'),
+			),
+		'priority' => 10, // Within the section.
+	  	'section' => 'colors', // Required, core or custom.
+	  	'label' => __( 'Select Footer Color Scheme', 'stedtnitz' ),
+	  	'description' => __( 'Chose the Footer color scheme for all webpages.', 'stedtnitz' ),	
+	) );
+
+	$wp_customize->add_setting( 'stedtnitz_menu_icon', array(
+	  'type' => 'theme_mod',
+	  'capability' => 'edit_theme_options',
+	  'default' => 'light',
+	  'transport' => 'postMessage', // or postMessage
+	) );
+	$wp_customize->add_control('stedtnitz_menu_icon', array(
+		'type' => 'select',
+		'choices' => array(
+			'light' => __('Light', 'stedtnitz'),
+			'dark' 	=> __('Dark', 'stedtnitz'),
+			),
+		'priority' => 10, // Within the section.
+	  	'section' => 'colors', // Required, core or custom.
+	  	'label' => __( 'Select Header Menu Button Color Scheme', 'stedtnitz' ),
+	  	'description' => __( 'Chose the Header Menu Button color scheme for all webpages.', 'stedtnitz' ),	
+	) );
 }
 add_action( 'customize_register', 'stedtnitz_customize_register' );
 
-function tcx_customizer_css() {
-	?>
-	<style type="text/css">
-		a.pp_button { border-color: <?php echo get_theme_mod( 'tcx_link_color' ); ?>; }
-	</style>
-	<?php
-}
-add_action( 'wp_head', 'tcx_customizer_css' );
+
+
 /**
  * Callback function to see if it is a blog page for viewing options.
  *
@@ -154,12 +197,122 @@ function stedtnitz_customize_partial_blogdescription() {
 }
 
 /**
- * Render the site tagline for the selective refresh partial.
+ * Render the site footer text for the selective refresh partial.
  *
  * @return void
  */
 function stedtnitz_customize_partial_footer_text() {
 	echo get_theme_mod('footer_text', 'Powered By Wordpress' );
+}
+
+function stedtnitz_header_bar_color_callback(){
+	?>
+	<div class="header <?php echo get_theme_mod('stedtnitz_header_bar_color', 'light'); ?>">
+	<?php 
+	if (has_custom_logo()) {
+		the_custom_logo();
+	}else{
+		?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<h5 class="site-title"><?php bloginfo( 'name' ); ?></h5>
+		</a>
+		<?php 
+	}
+	?>
+	</div>
+	<?php
+}
+
+function stedtnitz_footer_color_callback(){
+	?>
+	<div class="container-fluid wrap <?php echo get_theme_mod('stedtnitz_header_bar_color', 'light'); ?>">
+		<div class="footer-row row with-gutter">
+
+
+			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 ">
+				<div class="sitemap row with-gutter">
+
+					<?php if ( is_active_sidebar( 'footer__right_1' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_1' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_2' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_2' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_3' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_3' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_4' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_4' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_5' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_5' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_6' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_6' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_7' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_7' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__right_8' ) ) { ?>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+						<?php dynamic_sidebar( 'footer__right_8' ); ?>
+					</div>
+					<?php } ?>
+				</div>
+			</div>
+
+
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 last-xs first-sm first-md first-lg">
+				<div class="aboutsite row with-gutter">
+					<?php if ( is_active_sidebar( 'footer__left_1' ) ) { ?>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<?php dynamic_sidebar( 'footer__left_1' ); ?>
+					</div>
+					<?php } ?>
+
+					<?php if ( is_active_sidebar( 'footer__left_2' ) ) { ?>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<?php dynamic_sidebar( 'footer__left_2' ); ?>
+					</div>
+					<?php } ?>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 last-xs last-sm">
+					<?php if ( is_active_sidebar( 'footer__full_bottom' ) ) { ?>
+					<?php dynamic_sidebar( 'footer__full_bottom' ); ?>
+					<?php } ?>
+
+				<div class="site-info">
+					 <?php 
+						echo get_theme_mod('footer_text', 'Proudly Powered By Inject Themes' );
+					  ?>
+				</div><!-- .site-info -->
+			</div>
+		</div>
+	</div> <!-- container-fluid -->
+	<?php
 }
 
 /**
